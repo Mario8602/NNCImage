e17e91b6811e361924af0d4266fa42909c88113b
Mario8602 <hellp8901@gmail.com>
2022-11-02 16:43:06 +0500

    Commit#2, add requirements.txt, Deleting comments

--- mysite/mainimages/models.py
+++ mysite/mainimages/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import UserManager
 from django.db import models
 from django.urls import reverse
 
-# from players.models import CustomUser
-
 from players.models import CustomUser
 
 
@@ -30,31 +28,6 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.categories}'
-
-
-# class User(AbstractBaseUser):
-#     email = models.EmailField(max_length=100, unique=True)
-#     username = models.CharField(max_length=100, unique=True)
-#     is_admin = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-#
-#     objects = UserManager()
-#
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = ['email']
-#
-#     def __str__(self):
-#         return f'{self.email}'
-#
-#
-# class Profile(models.Model):
-#     first_name = models.CharField(max_length=40)
-#     last_name = models.CharField(max_length=40)
-#     description = models.TextField()
-#     photo = models.ImageField(default='default.png', upload_to='user_photos/%Y/%m/%d')
-#
-#     def __str__(self):
-#         return f'{self.user.username}'
 
 
 class Comment(models.Model):
